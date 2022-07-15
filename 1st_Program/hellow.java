2eff979a3603fb6a13e4ef146f42613d671d2e2b
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-07-15 16:28:22 +0530

    .

--- 1st_Program/hellow.java
+++ 1st_Program/hellow.java
@@ -6,7 +6,22 @@ public class hellow {
     public static void main(String[] args) {
         int a = 5;
         a = 6;
+        char c = 'N';
+        boolean b = true;
+        byte b1 = 127;
+        short s1 = 8678;
+        long l1 = 5454545345454l;
+        float f1 = 6.5f;
+        double d1 = 7.8;
+
         System.out.println(a);
         System.out.println("Hi");
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
     }
 }
