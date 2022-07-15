af61b5b67f319c225a7c1fdd15149e6f9769098d
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-07-15 13:33:52 +0530

    .

---
+++ 1st_Program/hellow.java
@@ -0,0 +1,12 @@
+/**
+ * hellow
+ */
+public class hellow {
+
+    public static void main(String[] args) {
+        int a = 5;
+        a = 6;
+        System.out.println(a);
+        System.out.println("Hi");
+    }
+}
