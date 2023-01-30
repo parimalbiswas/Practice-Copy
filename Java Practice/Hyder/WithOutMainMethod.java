43bbb7ca3df70cc2cfda1e7fadf8e2b13727fc78
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-01-30 23:45:25 +0530

    .

---
+++ Java Practice/Hyder/WithOutMainMethod.java
@@ -0,0 +1,14 @@
+/**
+ * WithOutMainMethod
+ */
+public class WithOutMainMethod extends Object {
+
+    public static void aaa() {
+        int a = 0;
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        WithOutMainMethod.aaa();
+    }
+}
