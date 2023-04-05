c6ff86898c86a00e4548938d2018873cc1b92f78
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-04-05 10:31:13 +0530

    .

--- DSA/IMP - VS CODE/Main MEthod/Main2.java
+++
@@ -1,5 +0,0 @@
-public class Main2 {
-    public static final synchronized void main(String[] args) {
-        System.out.println("Jioooooooooooooooooooo2....");
-    }
-}
--- DSA/IMP - VS CODE/Stream/ObjectCheck.java
+++
@@ -1,3 +0,0 @@
-public class ObjectCheck extends Object{
-    
-}
